61f3b4ec8da6c96a67d2656743887d422f8c85d5
madhav <109799001+madhav037@users.noreply.github.com>
2023-08-24 22:54:03 +0530

    initial

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express()
+
+
+
+app.get('/', (req,res) => {
+    res.json("HEllo")
+    console.log("listening on port 5000")
+})
+app.listen(5000)
