241225f77c309601b466dc045af2e6609025de58
madhav <109799001+madhav037@users.noreply.github.com>
2023-08-28 19:07:31 +0530

    Login page front end done

--- adminpannel/src/Components/Login/Login.js
+++ adminpannel/src/Components/Login/Login.js
@@ -1,19 +1,28 @@
-import './Login.css'
+import "./Login.css";
 
 export default function Login() {
-    return (
-        <>
-            <div className='Logo-bar'>Netflix</div>
-            <div className='Signin-box'>
-                <div className='signin-text'></div>
-                <div className='email-phonenumber'></div>
-                <div className='password'></div>
-                <button className='sign-in'>Sign in</button>
-                <div>
-                    <input type='checkbox' className='remeber-me'/>Remember me
-                    <p className='need-help'>Need help?</p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="blackLayer"></div>
+      <div className="Logo-bar">
+        <img src="https://logohistory.net/wp-content/uploads/2023/05/Netflix-Logo.png" />
+      </div>
+      <div className="signinbox">
+        <div id="signin-text">Sign In</div>
+        <input
+          type="text"
+          id="email-phonenumber"
+          placeholder="Email or phone number"
+        />
+        <br />
+        <input type="password" id="password" placeholder="Password" />
+        <br />
+        <button id="sign-in">Sign in</button>
+        <div>
+          <input type="checkbox" id="remeber-me" /> Remember me
+          <p id="need-help">Need help?</p>
+        </div>
+      </div>
+    </>
+  );
 }
