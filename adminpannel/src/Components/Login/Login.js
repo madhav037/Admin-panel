4ed32bc3ef6be7e28e222b2c25ea81b73e9e4b96
madhav <109799001+madhav037@users.noreply.github.com>
2023-08-29 18:07:09 +0530

    non database login page done

--- adminpannel/src/Components/Login/Login.js
+++ adminpannel/src/Components/Login/Login.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
 import "./Login.css";
 
 export default function Login() {
+  useEffect(()=> {
+    fetch('http://localhost:5000')
+    .then(res => res.json())
+    .then(console.log)
+  },[])
+
+  let [emailNumber, setEmailNumber] = useState('')
+  let [password, setPassword] = useState('')
+
   return (
     <>
       <div className="blackLayer"></div>
       <div className="Logo-bar">
-        <img src="https://logohistory.net/wp-content/uploads/2023/05/Netflix-Logo.png" />
+        <img src="https://logohistory.net/wp-content/uploads/2023/05/Netflix-Logo.png" alt='logo'/>
       </div>
       <div className="signinbox">
         <div id="signin-text">Sign In</div>
@@ -13,11 +23,32 @@ export default function Login() {
           type="text"
           id="email-phonenumber"
           placeholder="Email or phone number"
+          onChange={(e) => {
+            setEmailNumber(emailNumber = e.target.value)
+          }}
         />
         <br />
-        <input type="password" id="password" placeholder="Password" />
+        <input type="password" id="password" placeholder="Password" onChange={(e) =>{
+          setPassword(password = e.target.value)
+        }}/>
         <br />
-        <button id="sign-in">Sign in</button>
+        <button id="sign-in" onClick={()=> {
+          fetch('http://localhost:5000/' , {
+            method : "POST",
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify({
+              emailNumber : emailNumber,
+              password : password
+            })
+          })
+          .then(res => res.json())
+            .then(data => {
+              if (data === 'success') {
+                // ! ADD Route to next page
+                console.log("add route to next page")
+              }
+            })
+        }}>Sign in</button>
         <div>
           <input type="checkbox" id="remeber-me" /> Remember me
           <p id="need-help">Need help?</p>
--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
+const cors = require('cors')
+const checkLoginDetails = require('./sigin.js')
 const app = express()
+
+app.use(express.json());
+app.use(cors())
+
 
 
 app.get('/', (req,res) => {
+    // res.json(database)
     res.json("HEllo")
     console.log("listening on port 5000")
+})
+
+app.post('/', (req,res) => {
+    const emailNumber = req.body.emailNumber
+    const password = req.body.password
+    if (checkLoginDetails(emailNumber,password)) {
+        res.json('success')
+    }else {
+        res.status(400).json("error login in")
+    }
 })
 app.listen(5000)
